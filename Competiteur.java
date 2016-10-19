be0bfbaaea342a5a515aae2cddf2ae2e22f8510b
res-xyz <aymeric.moulard@Telecom-bretagne.eu>
2016-10-19 16:05:25 +0200

    Premiers ajouts

---
+++ Competiteur.java
@@ -0,0 +1,39 @@
+package siteParis;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+
+public class Competiteur {
+
+
+   private String nomCompetiteur;
+	private LinkedList<Joueur> joueurs;
+	/** 
+	 * @uml.property name="competition1"
+	 * @uml.associationEnd multiplicity="(0 -1)" aggregation="composite" inverse="competiteur1:siteParis.Competition"
+	 * @uml.association name="participe à"
+	 */
+	/** 
+	 * @uml.property name="listeCompetitions"
+	 * @uml.associationEnd multiplicity="(0 -1)" ordering="true" inverse="competiteur:siteParis.Competition"
+	 * @uml.association name="participe"
+	 */
+	private LinkedList<Competition> listeCompetitions;
+   
+   public Competiteur(String nomCompetiteur) throws CompetitionException{
+      
+      if ( nomCompetiteur == null ) throw new CompetitionException();
+      if ( !nomCompetiteur.matches("[0-9A-Za-z-_]{4,}")) throw new CompetitionException();        
+      
+      
+      this.nomCompetiteur = nomCompetiteur;
+      this.listeCompetitions = new LinkedList<Competition>();
+      this.joueurs = new LinkedList<Joueur>();
+      
+      }
+   
+   
+
+
+}
---
+++ Competition.java
@@ -0,0 +1,74 @@
+package siteParis;
+
+
+import java.util.LinkedList;
+
+
+public class Competition {
+
+	
+	/*private LinkedList<Competiteur> listeCompetiteurs;
+	
+	private DateFrancaise dateCloture;
+	*/
+
+	/**
+	 * @uml.property  name="nomCompetition"
+	 */
+	private String nomCompetition;
+	private DateFrancaise dateCloture;
+   
+   private LinkedList<Competiteur> competiteurs;
+	
+	
+	public Competition(String nomCompetition, DateFrancaise dateCloture) throws CompetitionException{
+   
+      if ( nomCompetition == null ) throw new CompetitionException();
+      if ( !nomCompetition.matches("[0-9A-Za-z-._]{4,}")) throw new CompetitionException();        
+       
+   
+		this.nomCompetition = nomCompetition;
+		this.dateCloture = dateCloture;
+		this.competiteurs = new LinkedList<Competiteur>();
+	}
+	
+   
+   
+   
+	public String getNomCompetition() {
+		return nomCompetition;
+	}
+
+
+
+
+
+
+	/**
+	 * Setter of the property <tt>listeCompetiteurs</tt>
+	 * @param listeCompetiteurs  The competiteurs to set.
+	 * @uml.property  name="listeCompetiteurs"
+	 */
+	public void setListeCompetiteurs(LinkedList<Competiteur> listeCompetiteurs) {
+		competiteurs = listeCompetiteurs;
+	}
+
+	/**
+	 * Getter of the property <tt>listeCompetiteurs</tt>
+	 * @return   Returns the competiteurs.
+	 * @uml.property   name="listeCompetiteurs"
+	 */
+	public LinkedList<Competiteur> getListeCompetiteurs() {
+		return competiteurs;
+	}
+
+
+	
+
+
+
+
+
+
+
+}
